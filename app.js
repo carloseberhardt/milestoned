3e3ea8de5c7a4f1730a7806793a1e6bc07121929
Carlos Eberhardt <carlos.eberhardt@gmail.com>
2012-05-05 09:24:58 -0500

    [#29085399 #29085861] Google OpenID login and logout.

--- app.js
+++ app.js
@@ -8,28 +8,46 @@ var express = require('express')
   , GoogleStrategy = require('passport-google').Strategy
   , routes = require('./routes');
 
-
 var app = module.exports = express.createServer();
 
+// serialization for passport
+// when we have db setup this should change to just userid 
+// instead of complete profile
+passport.serializeUser(function(user,done) {
+  done(null,user);
+});
+passport.deserializeUser(function(obj,done) {
+  done(null, obj);
+});
+
 passport.use(new GoogleStrategy({
-  returnURL: 'http://milestoned.herokuapp.com/auth/google/return',
-  realm: 'http://milestoned.herokuapp.com/'
- },
- function(identifier, profile, done) {
-  User.findOrCreate({openId: identifier }, function(err, user) {
-    done(err,user);
-  });
-}
-));
+  //returnURL: 'http://milestoned.herokuapp.com/auth/google/return',
+  //realm: 'http://milestoned.herokuapp.com/'
+    returnURL: 'http://localhost:5000/auth/google/return',
+    realm: 'http://localhost:5000/'
+  },
+  function(identifier, profile, done) {
+    process.nextTick(function () {
+      // associate user to record in db
+      // return that user profile instead
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
+  }
+  ));
 
 // Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.logger());
+  app.use(express.cookieParser());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.session({secret: 'lookoutkidtheykeepitallhid'}));
   app.use(passport.initialize());
+  app.use(passport.session());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -48,6 +66,7 @@ app.get('/', routes.index);
 app.get('/logout', routes.logout);
 app.get('/login', routes.login);
 app.get('/nodes/:id', routes.nodes)
+app.get('/profile', ensureAuthenticated, routes.profile);
 
 app.get('/auth/google', passport.authenticate('google'));
 
@@ -57,3 +76,9 @@ app.get('/auth/google/return', passport.authenticate('google', {successRedirect:
 app.listen(process.env.PORT, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
+
+// simple method to protect routes
+function ensureAuthenticated(req,res,next) {
+  if (req.isAuthenticated()) {return next();}
+  res.redirect('/login');
+}
