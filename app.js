9d6f118688911b512849b3bcacb660d9090244d2
Carlos Eberhardt <carlos.eberhardt@gmail.com>
2012-05-05 08:47:48 -0500

    [#29085399 #29085861] Google OpenID login and logout.

--- app.js
+++ app.js
@@ -4,9 +4,23 @@
  */
 
 var express = require('express')
+  , passport = require('passport')
+  , GoogleStrategy = require('passport-google').Strategy
   , routes = require('./routes');
 
+
 var app = module.exports = express.createServer();
+
+passport.use(new GoogleStrategy({
+  returnURL: 'http://milestoned.herokuapp.com/auth/google/return',
+  realm: 'http://milestoned.herokuapp.com/'
+ },
+ function(identifier, profile, done) {
+  User.findOrCreate({openId: identifier }, function(err, user) {
+    done(err,user);
+  });
+}
+));
 
 // Configuration
 
@@ -15,6 +29,7 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(passport.initialize());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -30,6 +45,14 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/logout', routes.logout);
+app.get('/login', routes.login);
+app.get('/nodes/:id', routes.nodes)
+
+app.get('/auth/google', passport.authenticate('google'));
+
+app.get('/auth/google/return', passport.authenticate('google', {successRedirect: '/',
+        failureRedirect: '/login'}));
 
 app.listen(process.env.PORT, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-
 /*
- * GET home page.
+ * GET index
  */
+
+exports.index = function(req, res){
+    if (req.profile) {
+        res.send('Hello ' + req.profile.displayName + '. You can <a href="/logout">logout.</a>');
+    } else {
+        res.send('This is the index. You can <a href="/auth/google">Sign in with Google</a>.')    
+    }
+}
+
+exports.login = function(req,res) {
+    res.send('<a href="/auth/google">Sign In with Google</a>');
+}
+
+exports.logout = function(req, res){
+    console.log("entering logout.")
+    req.logOut();
+    res.redirect('/');
+};
+
 var request = require("superagent");
-exports.index = function(req,res){
+exports.nodes = function(req,res){
   request.post(process.env.NEO4J_REST_URL + '/cypher').send({
     query: 'START n=node(1) RETURN n.name'
   }).end(function(neo4jRes){
